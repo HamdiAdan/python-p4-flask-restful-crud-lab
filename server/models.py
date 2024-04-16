d277ea39a0d4a22f5a6df1264fb6a354f66aecae
Hamdi Adan <hhhaji013@gmail.com>
2024-04-16 09:18:56 +0300

    completed

--- server/models.py
+++ server/models.py
@@ -10,7 +10,6 @@ class Plant(db.Model, SerializerMixin):
     name = db.Column(db.String)
     image = db.Column(db.String)
     price = db.Column(db.Float)
-    is_in_stock = db.Column(db.Boolean)
-
+    
     def __repr__(self):
-        return f'<Plant {self.name} | In Stock: {self.is_in_stock}>'
+        return f'<Plant {self.name}, {self.image}, {self.price}>'
